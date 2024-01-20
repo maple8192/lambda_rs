b1bcbd15051e9848beb1122e71a969fb067d0471
maple8192 <57043077+maple8192@users.noreply.github.com>
2024-01-21 06:04:24 +0900

    program arguments for code path

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use crate::calculator::calculator::run;
 use crate::io::reader::read_file;
 use crate::parser::program::parse_program;
 use crate::token::tokenizer::tokenize;
+use std::env;
 use std::error::Error;
 
 mod calculator;
@@ -10,7 +11,9 @@ mod parser;
 mod token;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let code = read_file("test.lmd")?;
+    let args = env::args().collect::<Vec<String>>();
+    let path = args.get(1).unwrap();
+    let code = read_file(path)?;
     let tokens = tokenize(code)?;
     let program = parse_program(tokens)?;
     run(program);
